33e5b10380eebd3c44ce0769655643a38476b74c
Diwakar Shukla <shukladiwakar@johndeere.com>
2020-11-07 09:42:05 +0530

    lox

--- manage.py
+++ manage.py
@@ -31,5 +31,6 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    print("Diwakar Aplication")
     client.run(TOKEN)
     execute_from_command_line(sys.argv)
